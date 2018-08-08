0ab80cb3bf92daf6fa8e39e449c7c5ddfc7972b0
Michael <mik.robinson@icloud.com>
2018-08-08 13:11:06 +0100

    fixed php login

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,33 +1,40 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: mik
  * Date: 06/08/2018
  * Time: 21:08
  */
+ 
+
+
    include("config.php");
    session_start();
+   $error = "";
 
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
-
+		
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+	  
+      $sql = "SELECT id FROM testTable WHERE username = '" . $myusername . "' and password = '". $mypassword ."'";
+	  
       $result = mysqli_query($db,$sql);
+	  
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
+      
 
       $count = mysqli_num_rows($result);
+	  
 
       // If result matched $myusername and $mypassword, table row must be 1 row
 
       if($count == 1) {
-         session_register("myusername");
          $_SESSION['login_user'] = $myusername;
-
-         header("location: welcome.php");
+         header('location: welcome.php');
+		 exit();
       }else {
          $error = "Your Login Name or Password is invalid";
       }
@@ -69,7 +76,7 @@
                   <input type = "submit" value = " Submit "/><br />
                </form>
 
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error ?></div>
 
             </div>
 
